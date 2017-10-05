3921effdf0fa3bcae5c704bc8c42517885ee4f46
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-04 22:06:48 -0400

    adds data options

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import data from './../data/data.js';
 
 export default class GameView extends Component {
   constructor(props) {
     super(props);
+    let options = props.textOptions.map( (val) => {
+      if (data[val]) {
+        return data[val];
+      } else {
+        return null;
+      }
+    });
+    options = [].concat.apply([], options);
 
     this.state = {
-      textOptions: props.textOptions
+      selectedCategories: props.textOptions,
+      options: options
     };
   }
 
   render() {
     return (
       <div>
-        {this.state.textOptions}
+        {this.state.selectedCategories}
+        {this.state.options}
       </div>
     )
   }
---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+const data = {
+  letters: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+  numbers: '0123456789'.split(''),
+  symbols: '<>;\'"[]{}+=()&%$#@!-*:.,`'
+};
+
+export default data;
