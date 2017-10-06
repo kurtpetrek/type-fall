16b0c1c32ac83dd114df71851d8da0847a4aee3e
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-06 17:47:00 -0400

    starts styling gameOverView

--- src/components/GameOverView.js
+++ src/components/GameOverView.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Button from './Button';
 
+import ViewContainer from './ViewContainer';
+import InnerContainer from './InnerContainer';
+import AnimatedHeader from './AnimatedHeader';
+
 export default function GameOverView(props) {
   return (
-    <div>
-      <h1>Game Over!</h1>
-      <h2>Score: {props.score}</h2>
-      <h2>Highscore: {props.highScore}</h2>
-      <h2>Options: {props.selectedTextOptions}</h2>
-      <h2>Speed: {props.spawnRate}</h2>
-      <Button handleClick={props.onGameRestart}>
-          New Game
-      </Button>
-    </div>
+    <ViewContainer>
+      <InnerContainer>
+        <AnimatedHeader>Type Fall</AnimatedHeader>
+        <h2>Game Over!</h2>
+        <h3>Score: {props.score}</h3>
+        <h3>Highscore: {props.highScore}</h3>
+        <h3>Options: {props.selectedTextOptions}</h3>
+        <h3>Speed: {props.spawnRate}</h3>
+        <div style={{textAlign: 'right'}}>
+          <Button handleClick={props.onGameRestart}>
+              New Game
+          </Button>
+        </div>
+      </InnerContainer>
+    </ViewContainer>
   );
 }
--- src/components/InnerContainer.js
+++ src/components/InnerContainer.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const InnerContainer = styled.div`
   max-width: 750px;
+  width: 100%;
   border: 5px double #C6C6CB;
   padding: 1rem;
   position: relative;
