20c43215576a5a6f9c94e7929ac3153670d26db1
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-06 23:28:25 -0400

    out transition on gameover

--- src/components/GameOverView.js
+++ src/components/GameOverView.js
@@ -26,63 +26,86 @@ const GameOverContainer = styled.div`
   }
 `;
 
-export default function GameOverView(props) {
-  let options = props.selectedTextOptions.map((el, i, arr) => {
-    let tail = ', ';
-    if (i === arr.length - 1) {
-      tail = '';
-    }
-    let text = el.charAt(0).toUpperCase() + el.slice(1) + tail;
-    return text;
-  });
-
-  const highScoretext = props.score === props.highScore ? 'New Highscore!' : '';
-
-  let spawnSpeedText = '';
-  switch (props.spawnRate) {
-    case 10:
-      spawnSpeedText = 'Faster';
-      break;
-    case 15:
-      spawnSpeedText = 'Fast';
-      break;
-    case 20:
-      spawnSpeedText = 'Normal';
-      break;
-    case 25:
-      spawnSpeedText = 'Slow';
-      break;
-    case 30:
-      spawnSpeedText = 'Slower';
-      break;
-    default:
-      spawnSpeedText = '';
-      break;
+export default class GameOverView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animatingOut: false
+    };
+    this.props = props;
   }
 
-  const hardcoreText = props.hardcore ? 'Hardcore' : '';
+  restartGame = () => {
+    this.setState(prevState =>{
+      prevState.animatingOut = true;
+      return prevState;
+    });
 
-  const containerStyle = false ? {transform: 'translateY(-100vh)'} : {animation: 'scale-in 1s forwards'};
+    setTimeout(()=>{
+      this.props.onGameRestart();
+    }, 600)
 
-  return (
-    <ViewContainer>
-        <InnerContainer style={containerStyle}>
-          <AnimatedHeader>Type Fall</AnimatedHeader>
-          <GameOverContainer>
-            <h2>Game Over!</h2>
-            <h2>{highScoretext}</h2>
-            <h3>Score: {props.score}</h3>
-            <h3>Highscore: {props.highScore}</h3>
-            <h3>{hardcoreText}</h3>
-            <h3>Characters: {options}</h3>
-            <h3>Speed: {spawnSpeedText}</h3>
-            <div style={{textAlign: 'right'}}>
-              <Button handleClick={props.onGameRestart}>
-                  New Game
-              </Button>
-            </div>
-          </GameOverContainer>
-        </InnerContainer>
-    </ViewContainer>
-  );
+  }
+
+  render(){
+    let props = this.props;
+    let options = props.selectedTextOptions.map((el, i, arr) => {
+      let tail = ', ';
+      if (i === arr.length - 1) {
+        tail = '';
+      }
+      let text = el.charAt(0).toUpperCase() + el.slice(1) + tail;
+      return text;
+    });
+
+    const highScoretext = props.score === props.highScore ? 'New Highscore!' : '';
+
+    let spawnSpeedText = '';
+    switch (props.spawnRate) {
+      case 10:
+        spawnSpeedText = 'Faster';
+        break;
+      case 15:
+        spawnSpeedText = 'Fast';
+        break;
+      case 20:
+        spawnSpeedText = 'Normal';
+        break;
+      case 25:
+        spawnSpeedText = 'Slow';
+        break;
+      case 30:
+        spawnSpeedText = 'Slower';
+        break;
+      default:
+        spawnSpeedText = '';
+        break;
+    }
+
+    const hardcoreText = props.hardcore ? 'Hardcore' : '';
+
+    const containerStyle = this.state.animatingOut ? {top: '-150vh'} : {};
+
+    return (
+      <ViewContainer>
+          <InnerContainer style={containerStyle}>
+            <AnimatedHeader>Type Fall</AnimatedHeader>
+            <GameOverContainer>
+              <h2>Game Over!</h2>
+              <h2>{highScoretext}</h2>
+              <h3>Score: {props.score}</h3>
+              <h3>Highscore: {props.highScore}</h3>
+              <h3>{hardcoreText}</h3>
+              <h3>Characters: {options}</h3>
+              <h3>Speed: {spawnSpeedText}</h3>
+              <div style={{textAlign: 'right'}}>
+                <Button handleClick={this.restartGame}>
+                    New Game
+                </Button>
+              </div>
+            </GameOverContainer>
+          </InnerContainer>
+      </ViewContainer>
+    );
+  }
 }
