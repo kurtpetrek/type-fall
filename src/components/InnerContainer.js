abcac163fd17ef91aaf68db0834c92b98f000029
Kurt Petrek <kurtpetrek@gmail.com>
2017-10-25 23:28:47 -0400

    runs prettier

--- src/components/InnerContainer.js
+++ src/components/InnerContainer.js
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const InnerContainer = styled.div`
   max-width: 750px;
   width: 100%;
-  border: 5px double #C6C6CB;
+  border: 5px double #c6c6cb;
   padding: 1rem;
   top: 0;
   position: relative;
   background: white;
   margin: 1rem;
-  animation: slide-in forwards .5s;
-  transition: .5s;
+  animation: slide-in forwards 0.5s;
+  transition: 0.5s;
 
   @keyframes slide-in {
     0% {
