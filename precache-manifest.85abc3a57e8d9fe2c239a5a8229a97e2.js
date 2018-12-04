8234e9bb483fb6874713ab1afbc105b460a8f1f7
kpuipath <kurtpetrek@gmail.com>
2018-12-04 15:39:29 -0500

    Updates

---
+++ precache-manifest.85abc3a57e8d9fe2c239a5a8229a97e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dad2a457436a3eba6091",
+    "url": "/type-fall/static/js/runtime~main.dad2a457.js"
+  },
+  {
+    "revision": "f64b52104d99d529e3c2",
+    "url": "/type-fall/static/js/main.f64b5210.chunk.js"
+  },
+  {
+    "revision": "e4cdcac102debc35b31e",
+    "url": "/type-fall/static/js/1.e4cdcac1.chunk.js"
+  },
+  {
+    "revision": "f64b52104d99d529e3c2",
+    "url": "/type-fall/static/css/main.def61112.chunk.css"
+  },
+  {
+    "revision": "acd3ef36733a5cd13c69385e11cf2b92",
+    "url": "/type-fall/index.html"
+  }
+];
